ff05570dca96a7670b57d28c64951e5f8ed979e1
vggithub7 <51913783+vggithub7@users.noreply.github.com>
2019-11-07 16:43:44 +0530

    Add files via upload

---
+++ fourth_ml_lab_odd_even.py
@@ -0,0 +1,11 @@
+def odd_even(n):
+    if (n%2)==0:
+        print("even")
+ #       input("prompt: ")
+    else:
+        print("odd")
+#        input("prompt: ")
+print("enter a number")
+a=int(input())
+odd_even(a)
+input("prompt: ")    
